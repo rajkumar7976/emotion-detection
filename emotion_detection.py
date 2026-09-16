1bc11a88fa16ee55a81bbdb49949eafb9e360e4a
rajkumar7976 <rakeshjangid341510@gmail.com>
2026-09-17 02:23:40 +0530

    AI: fix repository issues

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers =  {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
